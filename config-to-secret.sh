5b8e8c17c520ee62945f48b6b17bb850ad2d783a
Mike Metral <metral@gmail.com>
2015-11-05 17:01:06 -0800

    Change sed flag

--- config-to-secret.sh
+++ config-to-secret.sh
@@ -10,13 +10,13 @@ if [ $# -lt $EXPECTEDARGS ]; then
 fi
 
 K8S_SERVICE_NAME=$1
-sed -e "s#{{K8S_SERVICE_NAME}}#${K8S_SERVICE_NAME}#g" ./nginx-template.env > nginx.env
+sed -i "s#{{K8S_SERVICE_NAME}}#${K8S_SERVICE_NAME}#g" ./nginx-template.env > nginx.env
 
 K8S_DNS_HOST=$2
-sed -e "s#{{K8S_DNS_HOST}}#${K8S_DNS_HOST}#g" ./nginx.env >> nginx.env
+sed -i "s#{{K8S_DNS_HOST}}#${K8S_DNS_HOST}#g" ./nginx.env >> nginx.env
 
 NGINX_PORT=$3
-sed -e "s#{{NGINX_PORT}}#${NGINX_PORT}#g" ./nginx.env >> nginx.env
+sed -i "s#{{NGINX_PORT}}#${NGINX_PORT}#g" ./nginx.env >> nginx.env
 
 BASE64_ENC=$(cat nginx.env | base64 --wrap=0)
 sed -e "s#{{config_data}}#${BASE64_ENC}#g" ./nginx-env-template.yaml > nginx-env.yaml
