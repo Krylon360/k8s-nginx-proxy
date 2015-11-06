57fbd9b45ba9598cf0c4c41012170307625f15d4
Mike Metral <metral@gmail.com>
2015-11-06 13:22:22 -0800

    cleanup

--- start.sh
+++ start.sh
@@ -11,17 +11,15 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
+
 source /etc/nginx-env/config
 
-echo "here0"
 # Env says we're using SSL 
 if [ -n "${ENABLE_SSL+1}" ] && [ "${ENABLE_SSL,,}" = "true" ]; then
-  echo "here0a"
   echo "Enabling SSL..."
   cp /usr/src/proxy_ssl.conf /etc/nginx/conf.d/proxy.conf
 else
   # No SSL
-  echo "here0b"
   echo "Enabling *Without* SSL..."
   cp /usr/src/proxy_nossl.conf /etc/nginx/conf.d/proxy.conf
 fi
@@ -44,28 +42,19 @@ fi
 
 show_val() { echo "${!1}"; }
 
-echo "here1"
-
 K8S_SERVICE_NAME_CAP=`echo $K8S_SERVICE_NAME | awk '{print toupper($0)}'`
-echo "here2 - $K8S_SERVICE_NAME_CAP"
 SERVICE_PORT="$K8S_SERVICE_NAME_CAP"_SERVICE_PORT
-echo "here3 - $SERVICE_PORT"
 TARGET_SERVICE="$K8S_SERVICE_NAME":"$(show_val $SERVICE_PORT)"
 echo "Target service: $TARGET_SERVICE"
 
-echo "here4"
 # Tell nginx the address and port of the service to proxy to
 sed -i "s/{{TARGET_SERVICE}}/${TARGET_SERVICE}/g;" /etc/nginx/conf.d/proxy.conf
 sed -i "s/{{K8S_DNS_HOST}}/${K8S_DNS_HOST}/g;" /etc/nginx/conf.d/proxy.conf
 sed -i "s/{{NGINX_PORT}}/${NGINX_PORT}/g;" /etc/nginx/conf.d/proxy.conf
 
-echo "here5"
-
-
 cat /etc/nginx-env/config
 echo "---------------------"
 cat /etc/nginx/conf.d/proxy.conf
-
 
 echo "Starting nginx..."
 nginx -g 'daemon off;'
